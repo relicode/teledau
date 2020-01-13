3bee9f501d47b80ef9474507bde1eec253a679e3
Anssi Herranen <anssi.herranen@iki.fi>
2020-01-13 11:03:49 +0700

    Refactored cf-template to their own directory

--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -1,14 +1,12 @@
-import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {  APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 
 import truth from '@utils/math'
 
 
-export const hello: APIGatewayProxyHandler = async (): Promise<APIGatewayProxyResult> => {
+export const hello = async (): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Truth: ${truth}`,
-    }, null, 2),
+    body: JSON.stringify({ truth }),
   }
 }
