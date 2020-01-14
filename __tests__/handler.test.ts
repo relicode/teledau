407ae293123b6c934cd4660d56599f6dad0792c0
Anssi Herranen <anssi.herranen@iki.fi>
2020-01-14 16:22:24 +0700

    Added aws-lambda-test-utils and fixed tests accordingly

--- __tests__/handler.test.ts
+++ __tests__/handler.test.ts
@@ -1,11 +1,16 @@
+import utils from 'aws-lambda-test-utils'
 import { hello } from '@functions/handler'
 
+const ev = utils.mockEventCreator.createAPIGatewayEvent({
+  body: JSON.stringify({ update: { message: 'TEST MESSAGE' } }),
+})
+
 test('if hello handler returns statuscode 200', async () => {
-  const response = await hello()
+  const response = await hello(ev)
   expect(response.statusCode).toEqual(200)
 })
 
 test('if hello handler returns body with truth of 42', async () => {
-  const response = await hello()
+  const response = await hello(ev)
   expect(JSON.parse(response.body).truth).toEqual(42)
 })
