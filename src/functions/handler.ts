c33788c55a759cc5c2643d3ffadbb10bfaa902b2
Anssi Herranen <anssi.herranen@iki.fi>
2020-01-14 14:51:12 +0700

    Added logging the message's update

--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -1,10 +1,15 @@
-import {  APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
+import { Update } from 'node-telegram-bot-api'
 
 import truth from '@utils/math'
 
 
-export const hello = async (): Promise<APIGatewayProxyResult> => {
+export const hello = async (ev: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const body = JSON.parse(ev.body)
+  const { update }: { update: Update } = body
+  console.log(JSON.stringify(update, null, 2))
+
   return {
     statusCode: 200,
     body: JSON.stringify({ truth }),
